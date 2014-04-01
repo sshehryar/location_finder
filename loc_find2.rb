ea42d1f93671884dd7953887a046fedd26cd4417
shehryar <ali.shehryar@live.com>
2014-04-01 23:47:12 +0500

    First Commit Second Program

---
+++ loc_find2.rb
@@ -0,0 +1,20 @@
+puts "This program  gives a location name if you enter coordinates, and gives coordinates when you enter a location name "
+
+require 'Geocoder'
+
+print "Press (1) to enter location or (2) to enter coordinates: "
+choice = gets.chomp 
+ if choice == '1'
+     print "Enter location: "
+     loc = gets.chomp
+     coords = Geocoder.coordinates(loc)
+     puts "The coordinates for #{loc} are: #{coords}"
+     
+elsif choice == '2'
+    print "Enter Coordinates: "
+     coord = gets.chomp
+     locat = Geocoder.search(coord)
+     puts "#{locat}"
+     
+end
+     
