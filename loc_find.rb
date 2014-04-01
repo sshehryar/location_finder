42a647d8b00e8f3403ed2c95ebde9c5d81f5f101
shehryar <ali.shehryar@live.com>
2014-04-01 23:24:54 +0500

    First Commit

---
+++ loc_find.rb
@@ -0,0 +1,24 @@
+puts "This is a crappy program used to find stuff via geocoder!"
+
+#The following two lines of code is used to fetch the external (remote) ip of user's pc from whatismyip.com
+require 'open-uri'
+remote_ip = open('http://whatismyip.akamai.com').read
+
+print "Your Remote ip-address is: #{remote_ip} \n"
+
+#just a remider nut-job to run ruby scripts use C:\ruby loc_finder.rb
+
+#code below initializes and uses geocoder gem
+require 'geocoder'
+
+addr = remote_ip.to_s
+
+loc = Geocoder.search(addr)
+
+puts "#{loc}"
+
+#coord = Geocoder.coordinates(addr)
+#cntry = loc.country_name
+#puts "Coordinates: #{coord}"
+#puts "Country: #{country}"    
+
